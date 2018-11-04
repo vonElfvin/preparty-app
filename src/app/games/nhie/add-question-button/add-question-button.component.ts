3468d54bac6d870ed819cdbaf886d250327e762c
Max Fischer <maxfi539@student.liu.se>
2018-11-04 11:59:49 +0100

    Move add-question-button functionality to own component. Add some small CSS fixes

---
+++ src/app/games/nhie/add-question-button/add-question-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-question-button',
+  templateUrl: './add-question-button.component.html',
+  styleUrls: ['./add-question-button.component.scss']
+})
+export class AddQuestionButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
