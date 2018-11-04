f4038ff145f4adccefb8785318477f4bf37c89f5
vonElfvin <elvin.granat@gmail.com>
2018-11-04 11:50:30 +0100

    #2 Refactor landing page styling with dynamic loading of games from firestore

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -2,26 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NhieModule } from './nhie/nhie.module';
 import { GamesComponent } from './games.component';
-import { MatFormFieldModule, MatButtonModule, MatInputModule, MatDividerModule } from '@angular/material';
+import { MatFormFieldModule, MatButtonModule, MatInputModule, MatDividerModule, MatCardModule, MatGridListModule } from '@angular/material';
 import { FireauthService } from '../core/firebase/fireauth/fireauth.service';
 import { FormsModule } from '@angular/forms';
 import { AliasComponent } from './alias/alias.component';
 
-import {GameInstanceService} from './shared/game-instance.service';
+import { GameInstanceService } from './shared/game-instance.service';
+import { GameService } from './shared/game.service';
 
 @NgModule({
   declarations: [GamesComponent, AliasComponent],
   imports: [
     CommonModule,
+    FormsModule,
+
+    // Material
     MatFormFieldModule,
     MatButtonModule,
     MatInputModule,
     MatDividerModule,
-    FormsModule,
+    MatCardModule,
+    MatGridListModule,
+
     // Game Modules
     NhieModule,
   ],
   providers: [
+    GameService,
     GameInstanceService,
     FireauthService]
 })
