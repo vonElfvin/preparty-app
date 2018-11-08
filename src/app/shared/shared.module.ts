39a02f1f5d05dd2dd62c712dbd3e2255babb217d
Gustav <gustav.svensk95@gmail.com>
2018-11-08 22:29:10 +0100

    #11 add button module to game

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DefaultSizeButtonComponent } from './default-size-button/default-size-button.component';
-import { MatIconModule, MatButtonModule } from '@angular/material';
+import { MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { InputFieldComponent } from './input-field/input-field.component';
 
 @NgModule({
   declarations: [
     PageNotFoundComponent,
-    DefaultSizeButtonComponent
+    DefaultSizeButtonComponent,
+    InputFieldComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [
-    DefaultSizeButtonComponent
+    DefaultSizeButtonComponent,
+    InputFieldComponent
   ]
 })
 export class SharedModule { }
