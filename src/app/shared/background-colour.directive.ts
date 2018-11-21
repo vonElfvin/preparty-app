625fac27684e01e7ef68b391a173916055a4ac0a
Gustav <gustav.svensk95@gmail.com>
2018-11-21 20:38:18 +0100

    fix unsubscribe

--- src/app/shared/background-colour.directive.ts
+++ src/app/shared/background-colour.directive.ts
@@ -2,7 +2,7 @@ import { Directive, ElementRef, OnDestroy } from '@angular/core';
 import { GameService } from '../games/shared/game.service';
 import { Game } from '../games/shared/game.model';
 import { Router, Event, NavigationEnd } from '@angular/router';
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 
 
 @Directive({
@@ -18,25 +18,26 @@ export class BackgroundColourDirective implements OnDestroy {
     el: ElementRef,
     private router: Router) {
 
-    this.gameService.game.subscribe((game: Game) => {
-
-      if ((typeof game.backgroundColor !== 'undefined') && this.router.url !== '/') {
-        el.nativeElement.style.backgroundColor = game.backgroundColor;
-      } else {
-        el.nativeElement.style.backgroundColor = this.accent;
-      }
-
-      this.router.events.subscribe((event: Event) => {
-        if (event instanceof NavigationEnd) {
-          console.log(event['urlAfterRedirects']);
-          if (event['urlAfterRedirects'] === '/') {
-            console.log("accent-nav");
-            el.nativeElement.style.backgroundColor = this.accent;
-          } else {
-            el.nativeElement.style.backgroundColor = game.backgroundColor;
-          }
+    this.subscription = this.gameService.game.subscribe((game: Game) => {
+      if (game) {
+        if ((typeof game.backgroundColor !== 'undefined') && this.router.url !== '/') {
+          el.nativeElement.style.backgroundColor = game.backgroundColor;
+        } else {
+          el.nativeElement.style.backgroundColor = this.accent;
         }
-      });
+
+        this.router.events.subscribe((event: Event) => {
+          if (event instanceof NavigationEnd) {
+            console.log(event['urlAfterRedirects']);
+            if (event['urlAfterRedirects'] === '/') {
+              console.log("accent-nav");
+              el.nativeElement.style.backgroundColor = this.accent;
+            } else {
+              el.nativeElement.style.backgroundColor = game.backgroundColor;
+            }
+          }
+        });
+      }
     });
   }
 
