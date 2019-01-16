858f3557afb9057edf6032e5b39d70b1a5519aa0
Gustav <gustav.svensk95@gmail.com>
2019-01-16 19:51:55 +0100

    fix routing

--- src/app/games/this-or-that/this-or-that.module.ts
+++ src/app/games/this-or-that/this-or-that.module.ts
@@ -4,13 +4,15 @@ import { ThisOrThatComponent } from './this-or-that.component';
 import { CountdownComponent } from './countdown/countdown.component';
 import { StatementCardComponent } from './statement-card/statement-card.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ClockComponent } from './clock/clock.component';
 
 
 @NgModule({
   declarations: [
     ThisOrThatComponent,
     CountdownComponent,
-    StatementCardComponent
+    StatementCardComponent,
+    ClockComponent
   ],
   imports: [
     CommonModule,
