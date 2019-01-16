96b6c3f9ed2fe37f6dc13573af1e78b058ebf820
Gustav <gustav.svensk95@gmail.com>
2019-01-16 19:44:58 +0100

    add basic ui for this or that

---
+++ src/app/games/this-or-that/statement-card/statement-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statement-card',
+  templateUrl: './statement-card.component.html',
+  styleUrls: ['./statement-card.component.scss']
+})
+export class StatementCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/games/this-or-that/this-or-that.module.ts
+++ src/app/games/this-or-that/this-or-that.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ThisOrThatComponent} from './this-or-that.component';
+import { ThisOrThatComponent } from './this-or-that.component';
+import { CountdownComponent } from './countdown/countdown.component';
+import { StatementCardComponent } from './statement-card/statement-card.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    ThisOrThatComponent
-
+    ThisOrThatComponent,
+    CountdownComponent,
+    StatementCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
+
   ]
 })
 export class ThisOrThatModule { }
